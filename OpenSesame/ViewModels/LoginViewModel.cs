4f0ebad271ede7c830ffe144945c097c75787070
Michael Stonis <michael.stonis@eightbot.com>
2018-01-02 12:01:34 -0600

    migrating to reactive ui components for binding and response

--- OpenSesame/ViewModels/LoginViewModel.cs
+++ OpenSesame/ViewModels/LoginViewModel.cs
@@ -1,78 +1,132 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using MyQNET;
 using OpenSesame.Store;
 using OpenSesame.Utils;
 using OpenSesame.Views;
+using ReactiveUI;
 using Rendr.XFKit.Core.Util;
 using Xamarin.Forms;
 
 namespace OpenSesame.ViewModels
 {
-	public class LoginViewModel : INotifyPropertyChanged
+    public class LoginViewModel : ViewModelBase
 	{
-		string _username = ApplicationStore.Username;
-		public string Username {
-			get{return _username;}
-			set{
-				_username = value;
-				if(RegexUtil.ValidEmailAddress().Match(_username).Success)
-					OnPropertyChanged(nameof(ProfilePhotoUrl));
-			}
-		}
+		string _username;
+        public string Username
+        {
+            get { return _username; }
+            private set { this.RaiseAndSetIfChanged(ref _username, value); }
+        }
 
-		public string Password {get;set;} = ApplicationStore.Password;
+        string _password;
+        public string Password
+        {
+            get { return _password; }
+            set { this.RaiseAndSetIfChanged(ref _password, value); }
+        }
 
-		public ICommand SettingsCommand { get; set; }
-		public ICommand LoginCommand {get;set;}
-		public INavigation Nav {get;set;}
+        Uri _profilePhotoUrl;
+        public Uri ProfilePhotoUrl
+        {
+            get { return _profilePhotoUrl; }
+            private set { this.RaiseAndSetIfChanged(ref _profilePhotoUrl, value); }
+        }
 
-		IMyQService _svc;
+        ReactiveCommand<object, Unit> _tryLogin;
 
-		public LoginViewModel()
+        public ReactiveCommand<object, Unit> TryLogin
+        {
+            get { return _tryLogin; }
+            private set { this.RaiseAndSetIfChanged(ref _tryLogin, value); }
+        }
+
+        ReactiveCommand<object, Unit> _viewSettings;
+
+        public ReactiveCommand<object, Unit> ViewSettings
+        {
+            get { return _viewSettings; }
+            private set { this.RaiseAndSetIfChanged(ref _viewSettings, value); }
+        }
+
+        ObservableAsPropertyHelper<bool> _authenticationIsValid;
+
+        public bool AuthenticationIsValid
+        {
+            get => _authenticationIsValid?.Value ?? false;
+        }
+
+        public INavigation Nav {get;set;}
+
+        IMyQService _svc;
+
+        public LoginViewModel()
 		{
+            this.Username = ApplicationStore.Username;
+            this.Password = ApplicationStore.Password;
+
 			_svc = DependencyService.Get<IMyQService>(DependencyFetchTarget.GlobalInstance);
 
-			LoginCommand = new Command(OnLogin);
-			SettingsCommand = new Command(async () => await Nav.PushModalAsync(new SettingsView()));
+            this.WhenActivated(disposables => {
+                TryLogin = ReactiveCommand.CreateFromTask<object>(
+                    //The action to run for the command
+                    async x =>
+                    {
+                        ApplicationStore.Username = Username;
+                        ApplicationStore.Password = Password;
+
+                        (var success, var token) = await _svc.Login(Username, Password);
+
+                        if (success)
+                        {
+                            ApplicationStore.SecurityToken = token;
+                            await Nav.PopModalAsync();
+                        }
+                    },
+                    //When can this command run
+                    Observable.CombineLatest(
+                        this.WhenAnyObservable(x => x.TryLogin.IsExecuting).StartWith(false),
+                        this.WhenAnyValue(x => x.AuthenticationIsValid).StartWith(false),
+                        (isExecuting, authenticationIsValid) => !isExecuting && authenticationIsValid))
+                    .DisposeWith(disposables);
+
+
+                ViewSettings = ReactiveCommand.CreateFromTask<object>(
+                    //The action to run for the command
+                    async _ => await Nav.PushModalAsync(new SettingsView()),
+                    //When can this command run
+                    this.WhenAnyObservable(x => x.ViewSettings.IsExecuting).Select(isExecuting => !isExecuting))
+                    .DisposeWith(disposables);
+
+
+                this.WhenAnyValue(vm => vm.Username)
+                    .SubscribeOn(RxApp.TaskpoolScheduler)
+                    .Select(username => {
+                        if (string.IsNullOrEmpty(username) || !RegexUtil.ValidEmailAddress().Match(username).Success)
+                            return default(Uri);
+                        
+                        return Gravatar.GetImageUrl(username, 88 * 2);
+                    })
+                    .Catch(Observable.Return(default(Uri)))
+                    .ObserveOn(RxApp.MainThreadScheduler)
+                    .Subscribe(x => ProfilePhotoUrl = x)
+                    .DisposeWith(disposables);
+
+                this.WhenAnyObservable(x => x.Changed)
+                    .SubscribeOn(RxApp.TaskpoolScheduler)
+                    .Select(_ => Unit.Default)
+                    .StartWith(Unit.Default)
+                    .Select(x => !string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password))
+                    .ToProperty(this, x => x.AuthenticationIsValid, out _authenticationIsValid, false, scheduler: RxApp.MainThreadScheduler)
+                    .DisposeWith(disposables);
+            });
+
 		}
-
-		Uri profilePhotoUrl;
-		public Uri ProfilePhotoUrl
-		{
-			get
-			{
-				if(RegexUtil.ValidEmailAddress().Match(_username).Success)
-				{
-					profilePhotoUrl = Gravatar.GetImageUrl(_username, 88*2);
-				}
-				return profilePhotoUrl;
-			}
-
-		}
-
-		async void OnLogin()
-		{
-			ApplicationStore.Username = Username;
-			ApplicationStore.Password = Password;
-
-			(var success, var token) = await _svc.Login(Username, Password);
-
-			if(success)
-			{
-				ApplicationStore.SecurityToken = token;
-				await Nav.PopModalAsync();
-			}
-		}
-
-		public event PropertyChangedEventHandler PropertyChanged;
-
-		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-		{
-			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-		}
-
 	}
 }
--- OpenSesame/Views/LoginView.xaml.cs
+++ OpenSesame/Views/LoginView.xaml.cs
@@ -1,19 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using OpenSesame.ViewModels;
+using ReactiveUI;
+using ReactiveUI.XamForms;
 using Xamarin.Forms;
 
 namespace OpenSesame.Views
 {
-	public partial class LoginView : ContentPage
+    public partial class LoginView : ReactiveContentPage<LoginViewModel>
 	{
 		public LoginView()
 		{
 			InitializeComponent();
 
-			BindingContext = new LoginViewModel(){
+			this.BindingContext = new LoginViewModel(){
 				Nav = this.Navigation
 			};
+
+            this.WhenActivated(disposables => {
+                this.Bind(ViewModel, vm => vm.Username, ui => ui.username.Text)
+                    .DisposeWith(disposables);
+
+                this.Bind(ViewModel, vm => vm.Password, ui => ui.password.Text)
+                    .DisposeWith(disposables);
+
+                this.WhenAnyValue(x => x.ViewModel.ProfilePhotoUrl)
+                    .Select(uri => new UriImageSource{ Uri = uri })
+                    .BindTo(this, x => x.profilePhoto.Source)
+                    .DisposeWith(disposables);
+
+                this.BindCommand(ViewModel, vm => vm.TryLogin, ui => ui.login)
+                    .DisposeWith(disposables);
+                
+                this.BindCommand(ViewModel, vm => vm.ViewSettings, ui => ui.settings)
+                    .DisposeWith(disposables);
+            });
+
 		}
 	}
 }
