904472d688e4467868cd88661fe155b3c2618f9f
Michael Stonis <michael.stonis@eightbot.com>
2018-01-02 12:00:20 -0600

    core viewmodel object

---
+++ OpenSesame/ViewModels/ViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ReactiveUI;
+
+namespace OpenSesame.ViewModels
+{
+    public class ViewModelBase : ReactiveObject, ISupportsActivation
+    {
+        protected readonly ViewModelActivator _viewModelActivator = new ViewModelActivator();
+
+        public ViewModelActivator Activator
+        {
+            get { return _viewModelActivator; }
+        }
+    }
+}
